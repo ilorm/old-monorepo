17f12fc15d0489d75bfba7f3917386cf7aeb160f
Guillaume Daix <guillaumedaix@free.fr>
2018-01-23 13:54:11 +0100

    🔄 [MOD] (connector-mongo) Simplify complexity

--- packages/ilorm-connector-postgresql/lib/connector/convertQueryToSQLWhere.js
+++ packages/ilorm-connector-postgresql/lib/connector/convertQueryToSQLWhere.js
@@ -14,48 +14,62 @@ const operatorConversion = {
 };
 
 /**
+ * Browse input query and call handler per each combination of key, operator, value
+ * @param {Object} inputQuery The query to browse
+ * @param {Function} handler The handler will be called per each combination
+ * @returns {Void} Return nothing
+ */
+function browseQuery(inputQuery, handler) {
+  for (const key of Object.keys(inputQuery)) {
+    for (const operator of Object.keys(inputQuery[key])) {
+      const value = inputQuery[key][operator];
+
+      handler(key, operator, value);
+    }
+  }
+}
+
+/**
  * Generate a SQL WHERE clause from an ilorm Query
  * @param {Object} inputQuery The query to convert
  * @param {Number} [paramIndex=1] The index to handle parameter query
  * @returns {string} A SQL WHERE clause
  */
 const convertQueryToSQLWhere = (inputQuery, paramIndex = 1) => {
-  let query = '';
   const values = [];
+  const clauses = [];
+
+  /**
+   * Define a new SQL Operation
+   * @param {String} key The target key
+   * @param {String} sqlOperator The operator to apply
+   * @param {Mixing} value The value to compare
+   * @returns {void} return nothing
+   */
+  const addSQLOperation = (key, sqlOperator, value) => {
+    values.push(value);
+    clauses.push(`${key} ${sqlOperator} $${paramIndex++}`);
+  };
 
   if (!inputQuery) {
     return '';
   }
 
-  for (const key of Object.keys(inputQuery)) {
+  browseQuery(inputQuery, (key, operator, value) => {
+    if (operatorConversion[operator]) {
+      const sqlOperator = operatorConversion[operator];
 
-    for (const operator of Object.keys(inputQuery[key])) {
-      const value = inputQuery[key][operator];
-
-      values.push(value);
-
-      if (operatorConversion[operator]) {
-        const sqlOperator = operatorConversion[operator];
-
-        if (query.length > 0) {
-          query += ' AND ';
-        }
-
-        query += `${key} ${sqlOperator} $${paramIndex++}`;
-
-      } else if (operator === OPERATIONS.BETWEEN) {
-        query += `${key} >= $${paramIndex++} AND ${key} <= $${paramIndex++}`;
-        values.push(value);
-
-
-      } else {
-        throw new Error(`connector.PostgreSQL: UNDEFINED OPERATOR : ${operator}`);
-      }
+      addSQLOperation(key, sqlOperator, value);
+    } else if (operator === OPERATIONS.BETWEEN) {
+      addSQLOperation(key, '>=', value);
+      addSQLOperation(key, '<=', value);
+    } else {
+      throw new Error(`connector.PostgreSQL: UNDEFINED OPERATOR : ${operator}`);
     }
-  }
+  });
 
   return {
-    query,
+    query: clauses.join(' AND '),
     paramIndex,
     values,
   };
