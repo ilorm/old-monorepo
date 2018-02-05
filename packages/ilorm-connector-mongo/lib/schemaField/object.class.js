6d9120651a9edea3b3aa580dbc942ae29080cee2
Guillaume Daix <guillaumedaix@free.fr>
2018-02-05 17:33:55 +0100

    ✅ [ADD] (connector-mongo) Add test and debug array and object complex fields

--- packages/ilorm-connector-mongo/lib/schemaField/object.class.js
+++ packages/ilorm-connector-mongo/lib/schemaField/object.class.js
@@ -1,11 +1,60 @@
 'use strict';
 
-const { SchemaField, } = require('ilorm').Schema.Types;
+const SCHEMA = Symbol('Child schema');
+const { Schema, } = require('ilorm');
+const { SchemaField, } = Schema.Types;
 
 /**
  * Class representing a Object field
  */
 class ObjectField extends SchemaField {
+  /**
+   * Intantiate an object field with a child schema use to set rule to the child object
+   * @param {Object} childSchema The child object to use
+   */
+  constructor(childSchema) {
+    super();
+
+    this[SCHEMA] = new Schema(childSchema);
+  }
+
+
+  /**
+   * Return the query operation associated with the given schema field
+   * @param {Query} query the instance of query to use
+   * @return {Object} The query operations
+   */
+  getQueryOperations({ name, query, }) {
+    const queryOperations = super.getQueryOperations({
+      name,
+      query,
+    });
+
+    this[SCHEMA].properties.forEach(property => {
+      const propertyDefinition = this[SCHEMA].definition[property];
+      const queryOperationsConfig = {
+        query,
+        name: `${name}.${property}`,
+      };
+      const propertyValue = propertyDefinition.getQueryOperations(queryOperationsConfig);
+
+      Object.defineProperty(queryOperations, property, { value: propertyValue, });
+    });
+
+    return queryOperations;
+  }
+
+  /**
+   * Init the given instance field
+   * @param {Object} instance instance to init
+   * @param {String} field field to init
+   * @return {*} The initied field value
+   */
+  init(instance, field) {
+    const value = instance[field];
+
+    return this[SCHEMA].initInstance(value);
+  }
 }
 
 module.exports = ObjectField;
---
+++ packages/ilorm-connector-mongo/test/manipulateObject.test.js
@@ -0,0 +1,96 @@
+/* eslint-disable */
+
+const { expect, } = require('chai');
+const { MongoClient, } = require('mongodb');
+const ilorm = require('ilorm');
+
+const ilormMongo = require('../index');
+
+const DB_URL = 'mongodb://localhost:27017/ilorm';
+
+const { declareModel, newModel, } = ilorm;
+
+describe('ilorm-connector-mongodb', () => {
+  describe('test/manipulate object', () => {
+
+    let mongoClient;
+    let database;
+    let modelFactoryParams;
+    let Books;
+
+    before(async () => {
+
+      mongoClient = await MongoClient.connect(DB_URL);
+      database = await mongoClient.db('ilorm');
+
+      ilorm.use(ilormMongo);
+
+      // Required to do after the use, to change Schema behavior
+      const { Schema, } = require('ilorm');
+
+      const bookSchema = new Schema({
+        display: Schema.object({
+          title: Schema.string().required(),
+          description: Schema.string(),
+        }),
+      });
+
+      const MongoConnector = ilormMongo.fromClient(database);
+
+      modelFactoryParams = {
+        name: 'books',
+        schema: bookSchema,
+        connector: new MongoConnector({
+          collectionName: 'books',
+        }),
+      };
+
+      Books = newModel(modelFactoryParams);
+
+      declareModel('books', Books);
+    });
+
+    after(async () => {
+      await database.dropCollection('books');
+
+      await mongoClient.close();
+    });
+
+    it('Could insert object with map field', async() => {
+      const item = {
+        display: {
+          title: 'Le seigneur des anneaux',
+        }
+      };
+
+      const lordOfTheRing = new Books(item);
+
+      await lordOfTheRing.save();
+
+    });
+
+    it('Could update object with map field', async() => {
+      const lordOfTheRing = await Books.query()
+        .display.title.is('Le seigneur des anneaux')
+        .display.description.set('Lord of the rings in french')
+        .updateOne();
+    });
+
+    it('Could query object with map field', async() => {
+      const lordOfTheRing = await Books.query()
+        .display.title.is('Le seigneur des anneaux')
+        .findOne();
+
+      const jsonLordOfTheRing = await lordOfTheRing.getJson();
+
+      expect(jsonLordOfTheRing.display).to.be.deep.equal({
+        title: 'Le seigneur des anneaux',
+        description: 'Lord of the rings in french',
+      });
+    });
+  });
+});
+
+
+
+
