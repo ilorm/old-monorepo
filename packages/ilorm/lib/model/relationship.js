aa8fb5f2047d2bbc30324d92b7128d6810b6d789
Guillaume Daix <guillaumedaix@free.fr>
2018-02-08 09:16:48 +0100

    ✅ [ADD] (core) Start working on linkedWith operator and relationship store

---
+++ packages/ilorm/lib/model/relationship.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Primary = Symbol('PRIMARY_KEY');
+const references = new Map();
+
+const saveRelation = ({ modelA, referenceA, modelB, referenceB, }) => {
+  if (!references.has(modelA)) {
+    references.set(modelA, new Map());
+  }
+
+  references.get(modelA).set(modelB, {
+    referenceA,
+    referenceB,
+  });
+};
+
+const declareRelation = ({ modelSource, attributeSource, modelReference, }) => {
+  saveRelation({
+    modelA: modelSource,
+    referenceA: attributeSource,
+    modelB: modelReference,
+    referenceB: Primary,
+  });
+  saveRelation({
+    modelA: modelReference,
+    referenceA: Primary,
+    modelB: modelSource,
+    referenceB: attributeSource,
+  });
+};
+
+const getRelation = ({ modelSource, modelReference, }) => references.get(modelSource).get(modelReference);
+
+module.exports = {
+  declareRelation,
+  getRelation,
+};
