4522f1ce4c5a771dafc14febf0e89b855a37ed7a
Guillaume Daix <guillaumedaix@free.fr>
2018-02-14 09:39:26 +0100

    🔄 [MOD] (core) Convert all schemaField class to factory with dep inject

--- packages/ilorm/lib/schemaField/index.js
+++ packages/ilorm/lib/schemaField/index.js
@@ -1,27 +1,43 @@
 'use strict';
 
-const DateField = require('./date.class');
-const NumberField = require('./number.class');
-const ReferenceField = require('./reference.class');
-const SchemaField = require('./schemaField.class');
-const StringField = require('./string.class');
-const BooleanField = require('./boolean.class');
+const booleanFieldFactory = require('./boolean.factory');
+const dateFieldFactory = require('./date.factory');
+const numberFieldFactory = require('./number.factory');
+const referenceFieldFactory = require('./reference.factory');
+const { getSchemaField, } = require('./schemaField.class');
+const stringFieldFactory = require('./string.factory');
 
-const factory = {
-  boolean: () => new BooleanField(),
-  date: () => new DateField(),
-  number: () => new NumberField(),
-  reference: () => new ReferenceField(),
-  string: () => new StringField(),
+/**
+ * Generate fields from their factory
+ * @returns {Object} Returns field init
+ */
+const getFields = () => {
+  const SchemaField = getSchemaField();
+
+  const BooleanField = booleanFieldFactory(SchemaField);
+  const DateField = dateFieldFactory(SchemaField);
+  const NumberField = numberFieldFactory(SchemaField);
+  const ReferenceField = referenceFieldFactory(SchemaField);
+  const StringField = stringFieldFactory(SchemaField);
+
+  const factory = {
+    boolean: () => new BooleanField(),
+    date: () => new DateField(),
+    number: () => new NumberField(),
+    reference: () => new ReferenceField(),
+    string: () => new StringField(),
+  };
+
+  factory.Types = {
+    Boolean: BooleanField,
+    Date: DateField,
+    Number: NumberField,
+    Reference: ReferenceField,
+    SchemaField,
+    String: StringField,
+  };
+
+  return factory;
 };
 
-factory.Types = {
-  Boolean: BooleanField,
-  Date: DateField,
-  Number: NumberField,
-  Reference: ReferenceField,
-  SchemaField,
-  String: StringField,
-};
-
-module.exports = factory;
+module.exports = getFields;
---
+++ packages/ilorm/lib/schemaField/reference.factory.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { OPERATIONS, FIELDS, } = require('ilorm-constants').QUERY;
+const declareOperation = require('./helpers/declareOperation');
+
+/**
+ * Generate ReferenceField class from SchemaField
+ * @param {SchemaField} SchemaField to overload
+ * @returns {ReferenceField} The new Reference field
+ */
+const getReferenceField = SchemaField => {
+  /**
+   * Class representing a reference field
+   */
+  class ReferenceField extends SchemaField {
+
+    /**
+     * Return the query operation associated with the given schema field
+     * @param {Query} query the instance of query to use
+     * @return {Object} The query operations
+     */
+    getQueryOperations({query,}) {
+      const resultQueryOperations = super.getQueryOperations();
+
+      resultQueryOperations[OPERATIONS.LINKED_WITH] = declareOperation({
+        query,
+        operation: OPERATIONS.LINKED_WITH,
+        field: FIELDS.QUERY,
+        key: this.name,
+      });
+
+      return resultQueryOperations;
+    }
+
+    /**
+     * Cast a value to match the specific field or throw an exception
+     * @param {Mixed} value the value to cast
+     * @returns {Boolean} value the value casted to the specific schemaField type configuration
+     */
+    castValue(value) {
+      return value;
+    }
+  }
+
+  return ReferenceField;
+};
+
+module.exports = getReferenceField;
