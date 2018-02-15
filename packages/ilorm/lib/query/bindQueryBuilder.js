ec6a57ed407ff38d34d511f742213694e7c1f4d2
Guillaume Daix <guillaumedaix@free.fr>
2018-02-15 15:10:22 +0100

    🔄 [MOD] (core) Simplify queryBuilder

--- packages/ilorm/lib/query/bindQueryBuilder.js
+++ packages/ilorm/lib/query/bindQueryBuilder.js
@@ -4,24 +4,71 @@ const { FIELDS, SORT_BEHAVIOR, OPERATIONS, } = require('ilorm-constants').QUERY;
 const { LIMIT, QUERY, QUERY_OR, SELECT, SKIP, SORT, } = FIELDS;
 
 /**
- * Bind query builder function to the queryClass
- * @param {BaseQuery} queryClass The query class to bind the function
- * @returns {Function} Return a query builder
+ * Call onOperator if exists, per each operator bind with the current query
+ * @param {BaseQuery} queryClass The query class to use
+ * @param {Function} onOperator The function handler to call
+ * @returns {void} return nothing
  */
-const bindQueryBuilder = queryClass => ({ onOr, onOperator, onOptions, onSelect, onSort, }) => {
-  if (onOr) {
-    if (queryClass[QUERY_OR]) {
-      queryClass[QUERY_OR].forEach(onOr);
+const handleOnOperator = (queryClass, onOperator) => {
+  if (onOperator) {
+    const query = queryClass[QUERY];
+
+    for (const key of Object.keys(query)) {
+      for (const operator of Object.keys(query[key])) {
+        const value = query[key][operator];
+
+        onOperator(key, operator, value);
+      }
     }
   }
+};
 
+/**
+ * Call onOptions if exists, to pass options to the connector
+ * @param {BaseQuery} queryClass The query class to use
+ * @param {Function} onOptions The function handler to call
+ * @returns {void} return nothing
+ */
+const handleOptions = (queryClass, onOptions) => {
   if (onOptions) {
     onOptions({
       skip: queryClass[SKIP],
       limit: queryClass[LIMIT],
     });
   }
+};
 
+/**
+ * Call onOr if exists, per each or branch on the current query
+ * @param {BaseQuery} queryClass The query class to use
+ * @param {Function} onOr The function handler to call
+ * @returns {void} return nothing
+ */
+const handleOr = (queryClass, onOr) => {
+  if (onOr && queryClass[QUERY_OR]) {
+    queryClass[QUERY_OR].forEach(onOr);
+  }
+};
+
+/**
+ * Call onSelect if exists, per each selected field in the current query
+ * @param {BaseQuery} queryClass The query class to use
+ * @param {Function} onSelect The function handler to call
+ * @returns {void} return nothing
+ */
+const handleSelect = (queryClass, onSelect) => {
+  if (onSelect && queryClass[SELECT] && queryClass[SELECT].fields && queryClass[SELECT].fields.length > 0) {
+    queryClass[SELECT].fields.forEach(field => onSelect({ field, }));
+  }
+};
+
+/**
+ * Call onSort if exists, per each sort field on the current query
+ * @param {BaseQuery} queryClass The query class to use
+ * @param {Function} onSort The function handler to call
+ * @returns {void} return nothing
+ */
+const handleSort = (queryClass, onSort) => {
   if (onSort) {
     const sort = queryClass[SORT];
 
@@ -34,22 +81,19 @@ const bindQueryBuilder = queryClass => ({ onOr, onOperator, onOptions, onSelect,
       }
     }
   }
+};
 
-  if (onSelect && queryClass[SELECT] && queryClass[SELECT].fields && queryClass[SELECT].fields.length > 0) {
-    queryClass[SELECT].fields.forEach(field => onSelect({ field, }));
-  }
-
-  if (onOperator) {
-    const query = queryClass[QUERY];
-
-    for (const key of Object.keys(query)) {
-      for (const operator of Object.keys(query[key])) {
-        const value = query[key][operator];
-
-        onOperator(key, operator, value);
-      }
-    }
-  }
+/**
+ * Bind query builder function to the queryClass
+ * @param {BaseQuery} queryClass The query class to bind the function
+ * @returns {Function} Return a query builder
+ */
+const bindQueryBuilder = queryClass => ({ onOperator, onOptions, onOr, onSelect, onSort, }) => {
+  handleOnOperator(queryClass, onOperator);
+  handleOptions(queryClass, onOptions);
+  handleOr(queryClass, onOr);
+  handleSelect(queryClass, onSelect);
+  handleSort(queryClass, onSort);
 };
 
 module.exports = bindQueryBuilder;
