f123a474a1a58938355a63b3619ff31e29bb33e2
daix <guillaumedaix@free.fr>
2018-12-19 13:01:13 +0100

    [REFACTO] Move model constants from core to constants repo

--- packages/ilorm-constants/index.js
+++ packages/ilorm-constants/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const DATABASES = require('./lib/databases');
+const MODEL = require('./lib/model');
 const QUERY = require('./lib/query');
 
 module.exports = {
   DATABASES,
+  MODEL,
   QUERY,
 };
